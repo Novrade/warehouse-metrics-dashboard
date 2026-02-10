0c99ba292e9099aa35201fa5451fd6e1ff4045a5
Radoslaw NOWAKOWSKI <Radoslaw.Nowakowski@knapp.com>
2026-02-10 13:22:24 +0000

    Controller changed

---
+++ src/main/java/com/example/warehouse_metrics_dashboard/controller/DashboardController.java
@@ -0,0 +1,42 @@
+package com.example.warehouse_metrics_dashboard.controller;
+
+import com.example.warehouse_metrics_dashboard.service.DashboardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@org.springframework.stereotype.Controller
+public class DashboardController {
+
+    private final DashboardService dashboardService;
+
+    @Autowired
+    public DashboardController(DashboardService dashboardService) {
+        this.dashboardService = dashboardService;
+    }
+
+    @GetMapping("/dashboard")
+    public String home(Model model) {
+        model.addAttribute("lddCount", dashboardService.getLDDs());
+        model.addAttribute("area1FillLevel", dashboardService.getArea1FillLevel());
+        model.addAttribute("area2FillLevel", dashboardService.getArea2FillLevel());
+        model.addAttribute("releaseIssues", dashboardService.getReleaseProblems());
+        model.addAttribute("area3FillPercent", dashboardService.getArea3FillLevel());
+        model.addAttribute("area4FillPercent", dashboardService.getArea4FillLevel());
+        model.addAttribute("wip", dashboardService.getWip());
+        model.addAttribute("p2pPicks", dashboardService.getP2pPicks());
+        model.addAttribute("bufferPicks", dashboardService.getDbhPicks());
+        model.addAttribute("lanes", dashboardService.getPbLanes());
+        model.addAttribute("transporters", dashboardService.getTransportersDown());
+        model.addAttribute("lifts", dashboardService.getLiftsDown());
+        model.addAttribute("dbhLdds", dashboardService.getLDDsDBH());
+
+        return "home";
+    }
+
+    @GetMapping("/login")
+    public String loginPage() {
+        return "login";
+    }
+}
+
